e81739e39a45f28d20e201904cc57a42cb2de697
amazighii <abdessamadmazighi123@gmail.com>
2026-07-22 17:33:32 +0100

    fix: disable Eureka client registration and fetching in application properties

--- order-service/src/test/java/com/buy01/orders/OrdersApplicationTests.java
+++ order-service/src/test/java/com/buy01/orders/OrdersApplicationTests.java
@@ -1,10 +1,15 @@
 package com.buy01.orders;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.buy01.orders.repositories.OrderRepository;
 
 @SpringBootTest
 class OrdersApplicationTests {
+	@Mock
+	OrderRepository productRepository;
 
 	@Test
 	void contextLoads() {
